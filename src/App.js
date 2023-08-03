744375437b47589605f3f68fdbc0ef313a624e0a
Binyamin Roberts <jzroberts@hotmail.com>
2023-08-03 16:29:15 +0300

    form and controlled elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: true },
 ];
 
 function App() {
@@ -20,10 +22,47 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // guard clause in case someone tries to submit with nothing in field
+    if (!description) return;
+
+    // to create a new item for description
+    const newItem = { description, quality, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // To reset form after submit
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -32,7 +71,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -59,3 +98,12 @@ function Stats() {
 }
 
 export default App;
+
+// Controlled elements in React
+
+// In forms input fields state is maintained by DOM, but when using React we want React to maintain all the state to keep application in sync with DOM, so we must use controlled elements.
+
+// Three steps:
+// 1) Create a piece of state: useState()
+// 2) Put as value={state property}
+// 3) Use onChange()
