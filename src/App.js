d73eb380609ceeaea6c543359d32f6e3d9cc463a
Binyamin Roberts <jzroberts@hotmail.com>
2023-08-03 13:58:21 +0300

    list items, conditional line-through

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Charger", quantity: 12, packed: true },
+];
+
 function App() {
   return (
-    <>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
       <Stats />
-    </>
+    </div>
   );
 }
 
 function Logo() {
-  return <h1> Far Away </h1>;
+  return <h1> 🌴 Far Away 💼</h1>;
 }
 
 function Form() {
@@ -22,12 +28,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have x items on your list, and you already packed x (x%)</em>
     </footer>
   );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// Practice setting up app from scratch
-
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
